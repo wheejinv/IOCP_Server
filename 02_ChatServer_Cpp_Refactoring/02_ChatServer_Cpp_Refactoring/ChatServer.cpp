da316b795ebc1f701002ba049eff2b1220492036
whee <wheejinv@naver.com>
2018-03-08 16:55:34 +0900

    cpp version ing..

---
+++ 02_ChatServer_Cpp_Refactoring/02_ChatServer_Cpp_Refactoring/ChatServer.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "NetworkController.h"
+
+int main()
+{
+
+	NetworkController nc;
+	nc.InitWinSock();
+
+
+	return 0;
+}
--- 02_ChatServer_Cpp_Refactoring/02_ChatServer_Cpp_Refactoring/NetworkController.h
+++ 02_ChatServer_Cpp_Refactoring/02_ChatServer_Cpp_Refactoring/NetworkController.h
@@ -2,5 +2,12 @@
 
 class NetworkController
 {
+public:
+	// socket
 	void InitWinSock();
+	void CreateListenSocket();
+	void Bind(const u_short port);
+	void StartListening();
+	void WaitForAccept();
+	void CloseServer();
 };
