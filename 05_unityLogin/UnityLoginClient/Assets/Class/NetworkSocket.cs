a9ad584b5396f47dd71532027f48b6820affef44
whee <wheejinv@naver.com>
2018-04-14 17:52:01 +0900

    unity tcp client init

---
+++ 05_unityLogin/UnityLoginClient/Assets/Class/NetworkSocket.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net.Sockets;
+
+using UnityEngine;
+
+namespace Assets.Class
+{
+    public class NetworkSocket
+    {
+        public String host = "127.0.0.1";
+        public Int32 port = 2022;
+
+        TcpClient tcpSocket;
+        NetworkStream netStream;
+
+        StreamWriter socketWriter;
+        StreamReader socketReader;
+
+        Boolean isSetupSocket = false;
+        Boolean isCloseSocket = false;
+
+        private static NetworkSocket instance;
+
+        private NetworkSocket()
+        {
+            
+        }
+
+        public static NetworkSocket Instance
+        {
+            get
+            {
+                if( instance == null )
+                {
+                    instance = new NetworkSocket();
+                }
+
+                return instance;
+            }
+        }
+
+        public void ChangeHost( String host, Int32 port )
+        {
+            if( this.isSetupSocket == true )
+            {
+                Debug.Log("ChangeHost must run before SetupSocket function");
+                return;
+            }
+
+            this.host = host;
+            this.port = port;
+        }
+
+        public void SetupSocket()
+        {
+            try
+            {
+                if( this.isSetupSocket == true )
+                {
+                    Debug.Log("SetupSocket is already run");
+                    return;
+                }
+
+                this.isSetupSocket = true;
+
+                this.tcpSocket = new TcpClient(this.host, this.port);
+
+                this.netStream = tcpSocket.GetStream();
+
+                this.socketWriter = new StreamWriter(this.netStream);
+                this.socketReader = new StreamReader(this.netStream);
+            }
+            catch( Exception e )
+            {
+                // Something went wrong
+                Debug.Log("Socket error: " + e);
+            }
+        }
+
+        public void SendSocket(String line)
+        {
+            if( this.isCloseSocket == true )
+            {
+                return;
+            }
+
+            line += "\r\n";
+            this.socketWriter.Write(line);
+            this.socketWriter.Flush();
+        }
+
+        public String RecvSocket()
+        {
+            if( this.isCloseSocket == true )
+            {
+                return "";
+            }
+
+            if( this.netStream.DataAvailable)
+            {
+                return this.socketReader.ReadLine();
+            }
+
+            return "";
+        }
+
+        public void CloseSocket()
+        {
+            if (this.isCloseSocket == true)
+            {
+                return;
+            }
+
+            socketWriter.Close();
+            socketReader.Close();
+            tcpSocket.Close();
+            this.isCloseSocket = true;
+        }
+    }
+}
---
+++ 05_unityLogin/UnityLoginClient/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Assets.Class;
+
+public class GameManager : MonoBehaviour {
+
+    [Header("LoginPanel")]
+    public InputField InputField_ID;
+    public InputField InputField_Password;
+
+    // Use this for initialization
+    void Start () {
+        NetworkSocket.Instance.SetupSocket();
+
+        NetworkSocket.Instance.SendSocket("test msg");
+
+    }
+
+    void Update()
+    {
+        string recvString = NetworkSocket.Instance.RecvSocket();
+
+        if( recvString != "" )
+        {
+            Debug.Log("recv from server : " + recvString);
+        }
+    }
+
+    public void LoginBtn()
+    {
+        StartCoroutine(LoginCo());
+    }
+
+    IEnumerator LoginCo()
+    {
+        Debug.Log(InputField_ID.text);
+        Debug.Log(InputField_Password.text);
+
+        yield return null;
+    }
+}
